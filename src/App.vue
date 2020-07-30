31e7f3b2832bfb7ad4551f119993826fe6009d84
Zijie Wu <noojeff@gmail.com>
2020-07-30 11:35:34 -0700

    Upgrade

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,6 @@
     <div>
         <Setting></Setting>
         <Game></Game>
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114654492-4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-114654492-4');
-</script>
-
     </div>
 </template>
 
